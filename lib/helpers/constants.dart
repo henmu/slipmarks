da2e4dfa7d6b2c89c07a5df3ef7e22dcd2435579
henmu <97477096+henmu@users.noreply.github.com>
2023-07-26 16:31:02 +0300

    fix access_token
    # Conflicts:
    #	lib/services/auth_service.dart

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,5 +1,6 @@
 const AUTH0_DOMAIN = String.fromEnvironment('AUTH0_DOMAIN');
 const AUTH0_CLIENT_ID = String.fromEnvironment('AUTH0_CLIENT_ID');
+const AUTH0_AUDIENCE = String.fromEnvironment('AUTH0_AUDIENCE');
 const AUTH0_ISSUER = 'https://$AUTH0_DOMAIN';
 const BUNDLE_IDENTIFIER = 'com.slipmarks.app';
 const AUTH0_REDIRECT_URI = '$BUNDLE_IDENTIFIER://login-callback';
