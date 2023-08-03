c524e72439d1ce51b24491d241cf68dc0f1ffb22
henmu <thevispil@gmail.com>
2023-08-03 09:50:51 +0300

    removed local .json file from early testing

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -58,124 +58,16 @@ class _FavoritesState extends State<Favorites> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.transparent,
-      body: FutureBuilder<List<BookmarkInfo>>(
-        future: _linksFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasError) {
-            return const Center(
-              child: Text('Error loading data'),
-            );
-          } else {
-            final links = snapshot.data ?? [];
-            return ListView.builder(
-              itemCount: links.length,
-              itemBuilder: (context, index) {
-                final link = links[index];
-                return GestureDetector(
-                  onTap: () => _launchURL(link.url),
-                  child: SwipeableTile.card(
-                    borderRadius: 10,
-                    color: const Color(0xFF282828),
-                    // color: Colors.white,
-                    key: UniqueKey(),
-                    swipeThreshold: 0.9,
-                    direction: SwipeDirection.horizontal,
-                    onSwiped: (_) {},
-                    backgroundBuilder: (context, direction, progress) {
-                      if (direction == SwipeDirection.endToStart) {
-                        return Container(
-                          color: Colors.red,
-                        );
-                      } else if (direction == SwipeDirection.startToEnd) {
-                        return Container(
-                          color: Colors.green,
-                        );
-                      }
-                      return Container();
-                    },
-                    horizontalPadding: 16,
-                    verticalPadding: 8,
-                    shadow: BoxShadow(
-                      color: Colors.black.withOpacity(0.35),
-                      blurRadius: 4,
-                      offset: const Offset(2, 2),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              vertical: 9, horizontal: 11),
-                          child: Image.asset(
-                            link.iconUrl, // Assuming the icon images are inside the assets folder
-                            width: 32,
-                            height: 32,
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              //Links title
-                              Padding(
-                                padding: const EdgeInsets.only(top: 12),
-                                child: Text(
-                                  link.name,
-                                  // style: const TextStyle(color: Colors.white),
-                                  overflow: TextOverflow.clip,
-                                  maxLines: 1,
-                                ),
-                              ),
-                              //Links URL
-                              Padding(
-                                padding: const EdgeInsets.only(right: 1),
-                                child: Text(
-                                  link.url,
-                                  style: const TextStyle(
-                                      color: Color(0xFF979797), fontSize: 12),
-                                  overflow: TextOverflow.clip,
-                                  maxLines: 1,
-                                ),
-                              ),
-                              //Footer
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.end,
-                                children: [
-                                  //Timestamp
-                                  Text(
-                                    link.createdAt,
-                                    textAlign: TextAlign.end,
-                                    style: const TextStyle(
-                                        color: Color(0xFFB1B1B1), fontSize: 12),
-                                  ),
-                                  //Triple dot
-                                  const Padding(
-                                    padding:
-                                        EdgeInsets.symmetric(horizontal: 8),
-                                    child: Icon(
-                                      Icons.more_horiz,
-                                      color: Color(0xFFB1B1B1),
-                                      size: 18,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            );
-          }
-        },
+      body: Center(
+        child: Text(
+          'This is the Search page',
+          style: TextStyle(
+            fontFamily: 'Inter',
+            // color: Colors.white,
+          ),
+        ),
       ),
     );
   }
