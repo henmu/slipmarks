36fd7618465c1df3a949700d52aa3c5f92bd886e
Sakari Mursu <mskri@mursu.dev>
2023-08-05 22:14:10 +0300

    Send device data to server for notifications
    Device id must be unique that stays the same in the device. `android_id` should work for android and `device_info_plus` should provide id for ios (not tested).

--- lib/services/messaging_service.dart
+++ lib/services/messaging_service.dart
@@ -1,10 +1,14 @@
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:android_id/android_id.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:slipmarks/helpers/constants.dart';
+import 'package:http/http.dart' as http;
+import 'package:slipmarks/services/auth_service.dart';
 
 class MessagingService {
   static String? fcmToken;
@@ -40,12 +44,14 @@ class MessagingService {
         await secureStorage.write(key: FCM_TOKEN_KEY, value: fcmToken);
         print("fcm token is new, sending it to server");
 
-        var model = await getDeviceModel();
-        print("Device model: $model");
-        // TODO: send token + device model + platform to server
+        if (fcmToken != null) {
+          sendTokenToServer(fcmToken!);
+        }
       } else {
         debugPrint("fcm token was the same as old one, not updating to server");
       }
+
+      sendTokenToServer(fcmToken ?? "");
 
       // Handling background messages using the specified handler
       FirebaseMessaging.onBackgroundMessage(
@@ -70,21 +76,54 @@ class MessagingService {
     }
   }
 
-  Future<String> getDeviceModel() async {
+  Future<void> sendTokenToServer(String pushToken) async {
+    var (id, model) = await getDeviceModelAndUniqueId();
+    print("Device id: $id");
+    print("Device model: $model");
+    print("Push token: $fcmToken");
+    print("Platform: ${Platform.operatingSystem.toUpperCase()}");
+
+    try {
+      final accessToken = AuthService.instance.accessToken;
+      final url = Uri.parse('$SERVER_HOST/devices');
+      await http.put(url,
+          headers: {
+            'Authorization': 'Bearer $accessToken',
+            'Content-Type': 'application/json',
+          },
+          body: json.encode({
+            "device_id": id,
+            "device_name": model,
+            "push_token": pushToken,
+            "platform": Platform.operatingSystem.toUpperCase(),
+          }));
+    } catch (e) {
+      print('Error creating device: $e');
+    }
+  }
+
+  Future<(String?, String)> getDeviceModelAndUniqueId() async {
+    late String? id;
     late String model;
 
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
     if (Platform.isIOS) {
       IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      id = iosInfo.identifierForVendor;
       model = iosInfo.utsname.machine;
     } else if (Platform.isAndroid) {
       AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+      const androidIdPlugin = AndroidId();
+      final String? androidId = await androidIdPlugin.getId();
+
+      id = androidId;
       model = androidInfo.model;
     } else {
+      id = 'unknown';
       model = 'unknown';
     }
 
-    return model;
+    return (id, model);
   }
 }
 
