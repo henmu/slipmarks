fa4919fdcf12ce2de9077a4a739f629476fcc18f
Sakari Mursu <mskri@mursu.dev>
2023-08-05 14:16:12 +0300

    Handle background messages

--- lib/services/messaging_service.dart
+++ lib/services/messaging_service.dart
@@ -29,5 +29,15 @@ class MessagingService {
     fcmToken = await _fcm.getToken();
     print('fcmToken: $fcmToken');
 
+
+    // Handling background messages using the specified handler
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 }
 
+// Handler for background messages
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  debugPrint('Handling a background message: ${message.notification!.title}');
+}
